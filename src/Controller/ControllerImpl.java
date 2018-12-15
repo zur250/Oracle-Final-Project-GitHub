14284181ccb36d58a5710fe13987a7a6e327f602
guyshakk <45487573+guyshakk@users.noreply.github.com>
2018-12-15 19:51:19 +0200

    Update ControllerImpl.java

--- src/Controller/ControllerImpl.java
+++ src/Controller/ControllerImpl.java
@@ -2,7 +2,8 @@ package Controller;
 
 import View.ViewInterface;
 
-public class ControllerImpl implements ControllerInterface {
+public class ControllerImpl implements ControllerInterface {//should there be a "change view" function?
+	//what about update user's role? from worker to customer in case he was fired for example...
 
 	@Override
 	public void Connect(String userName, String password) {
@@ -11,49 +12,52 @@ public class ControllerImpl implements ControllerInterface {
 	}
 
 	@Override
-	public void register(String userName, String password, int balance, String phoneNumber) {
+	public void register(String userName, String password, int balance, String phoneNumber) { //why int balance? not double? we are dealing with discounts
+		//that would turn the price into fractions
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void disconnect() {
+	public void disconnect() {//shouldn't this function know who is connected?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void addProductToCart(int productID, int ammount) {
+	public void addProductToCart(int productID, int ammount) {//how do I know which cart? is the user stored in the controller
+		//or should it be passed as an argument in this function?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void removeProductFromCart(int productID, int ammount) {
+	public void removeProductFromCart(int productID, int ammount) {//same question as above
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void updateRoleDiscount(int percentage) {
+	public void updateRoleDiscount(int percentage) {//which role?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void updateProdctInStore(int productID, String productName, int price, int ammountInStock) {
+		//is there a reason for the product ID to be int? shouldn't it be string? we don't make any calculations with this number...
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void deleteProductFromStore(int productID) {
+	public void deleteProductFromStore(int productID) {//same as above
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void addProductToStore(String productName, int price, int ammountInStock) {
+	public void addProductToStore(String productName, int price, int ammountInStock) {//is the price int?
 		// TODO Auto-generated method stub
 		
 	}
@@ -65,13 +69,13 @@ public class ControllerImpl implements ControllerInterface {
 	}
 
 	@Override
-	public void updateUserPassword(String currentPassword, String newPassword) {
+	public void updateUserPassword(String currentPassword, String newPassword) {//which user?
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void registerView(ViewInterface IView) {
+	public void registerView(ViewInterface IView) {//wrong interface
 		// TODO Auto-generated method stub
 		
 	}
