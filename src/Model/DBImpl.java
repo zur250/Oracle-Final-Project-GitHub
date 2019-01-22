6842d8f42f978ddaf5234e29a485f9dab1421bea
Zur <zur1995@gmail.com>
2019-01-22 21:01:49 +0200

    added the rest of the users pkg function. at the moment missing get all users and get all roles function

--- src/Model/DBImpl.java
+++ src/Model/DBImpl.java
@@ -2,6 +2,9 @@ package Model;
 
 import java.sql.*;
 
+import oracle.jdbc.OracleCallableStatement;
+import oracle.jdbc.OracleTypes;
+
 public class DBImpl implements DBInterface {
 
 	private Connection conn;
@@ -65,8 +68,22 @@ public class DBImpl implements DBInterface {
 	}
 
 	@Override
-	public User get_user(String userName) {
-		// TODO Auto-generated method stub
+	public User get_user(String userName) throws SQLException {
+		try {
+		User cur_User = null;
+		CallableStatement stmt = conn.prepareCall("BEGIN Zur.users_pkg.get_user(?,?); END;");
+		stmt.setString(1, userName);
+		stmt.registerOutParameter(2, OracleTypes.CURSOR);
+		stmt.execute();
+	    ResultSet rs = ((OracleCallableStatement)stmt).getCursor(2);
+	    while (rs.next()) {
+	        cur_User = new User(rs.getString("username"), rs.getString("pass"), rs.getInt("phone"), rs.getDate("joindate"), rs.getInt("roleid"), rs.getDouble("balance"));
+	      }
+		return cur_User;
+		}
+		catch(Exception e){
+			System.out.println(e.getMessage()+"User does not exsist?");
+		}
 		return null;
 	}
 	
@@ -75,7 +92,13 @@ public class DBImpl implements DBInterface {
 		DBImpl d = new DBImpl();
 		d.dbConnect("admin", "147258");
 		try {
-			d.register_user("test12", "test12345", 1, 1234.2,526442708);
+			User u = d.get_user("test12");
+			System.out.println(u.getUserName());
+			System.out.println(u.getPassword());
+			System.out.println(u.getPhoneNumber());
+			System.out.println(u.getBalance());
+			System.out.println(u.getHire_date());
+			System.out.println(u.getRoleID());
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- src/Model/DBInterface.java
+++ src/Model/DBInterface.java
@@ -10,7 +10,7 @@ public interface DBInterface {
 	void delete_user(String userName) throws SQLException;
 	void change_password(String userName,String newPass) throws SQLException;
 	void change_discount(String roleName,int discountValue) throws SQLException;
-	User get_user(String userName);
+	User get_user(String userName) throws SQLException;
 	/*
     FUNCTION get_user(username users.username%type) return users%rowtype;
     */
